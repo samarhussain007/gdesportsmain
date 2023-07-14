1360054abdd8955ce41a6fba9374a5c1344d4dd8
its-saif <saif41428@GMAIL.COM>
2023-07-14 12:19:28 +0530

    Gamersground Date Update

--- gd-form copy 2/script.js
+++ gd-form copy 2/script.js
@@ -1,30 +1,30 @@
 let timer;
 
 const compareDate = new Date();
-var countDownDate = new Date("JUL 15, 2023 16:37:52").getTime();
+var countDownDate = new Date("AUG 4, 2023 16:37:52").getTime();
 
 compareDate.setDate(compareDate.getDate() + 11); //just for this demo today + 7 days
 
 var myfunc = setInterval(function () {
-	// code goes here
-	var now = new Date().getTime();
-	var timeleft = countDownDate - now;
+  // code goes here
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
 
-	var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-	document.querySelector(".day").textContent = String(days).padStart(2, 0);
-	document.querySelector(".hour").textContent = String(hours).padStart(2, 0);
-	document.querySelector(".minute").textContent = String(minutes).padStart(
-		2,
-		0
-	);
-	document.querySelector(".second").textContent = String(seconds).padStart(
-		2,
-		0
-	);
+  document.querySelector(".day").textContent = String(days).padStart(2, 0);
+  document.querySelector(".hour").textContent = String(hours).padStart(2, 0);
+  document.querySelector(".minute").textContent = String(minutes).padStart(
+    2,
+    0
+  );
+  document.querySelector(".second").textContent = String(seconds).padStart(
+    2,
+    0
+  );
 }, 1000);
 
 // function timeBetweenDates(toDate) {
@@ -49,44 +49,44 @@ let formHtml = [];
 console.log(formHtml);
 
 function confirm(e) {
-	e.preventDefault();
-	const edit = document.querySelectorAll(".edit");
-	edit.forEach((el) => {
-		el.style.display = "flex";
-		el.addEventListener("click", function (e) {
-			e.preventDefault();
-			el.closest(".form")
-				.querySelectorAll("input")
-				.forEach((el) => {
-					el.readOnly = !el.readOnly;
-					el.classList.toggle("hidden");
-				});
-		});
-	});
-	document.querySelector(".form-btn-confirm").style.display = "none";
-	document.querySelector(".form-btn").style.display = "flex";
-	document.querySelectorAll(".form").forEach((el) => {
-		el.querySelectorAll("input").forEach((el) => {
-			el.readOnly = "true";
-			el.classList.add("hidden");
-		});
-	});
+  e.preventDefault();
+  const edit = document.querySelectorAll(".edit");
+  edit.forEach((el) => {
+    el.style.display = "flex";
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      el.closest(".form")
+        .querySelectorAll("input")
+        .forEach((el) => {
+          el.readOnly = !el.readOnly;
+          el.classList.toggle("hidden");
+        });
+    });
+  });
+  document.querySelector(".form-btn-confirm").style.display = "none";
+  document.querySelector(".form-btn").style.display = "flex";
+  document.querySelectorAll(".form").forEach((el) => {
+    el.querySelectorAll("input").forEach((el) => {
+      el.readOnly = "true";
+      el.classList.add("hidden");
+    });
+  });
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-		const myForm = event.target;
-		const formData = new FormData(myForm);
+    const myForm = event.target;
+    const formData = new FormData(myForm);
 
-		fetch("/", {
-			method: "POST",
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: new URLSearchParams(formData).toString(),
-		})
-			.then(() => (document.location.href = "../confirm/register.html"))
-			.catch((error) => alert(error));
-	};
-	document.querySelector(".form-box").addEventListener("submit", handleSubmit);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => (document.location.href = "../confirm/register.html"))
+      .catch((error) => alert(error));
+  };
+  document.querySelector(".form-box").addEventListener("submit", handleSubmit);
 }
 document.querySelector(".form-box").addEventListener("submit", confirm);
 
@@ -108,19 +108,19 @@ document.querySelector(".form-box").addEventListener("submit", confirm);
 // document.querySelector("form").addEventListener("submit", handleSubmit);
 
 function checkFlexGap() {
-	var flex = document.createElement("div");
-	flex.style.display = "flex";
-	flex.style.flexDirection = "column";
-	flex.style.rowGap = "1px";
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-	flex.appendChild(document.createElement("div"));
-	flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
-	document.body.appendChild(flex);
-	var isSupported = flex.scrollHeight === 1;
-	flex.parentNode.removeChild(flex);
-	// console.log(isSupported);
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  // console.log(isSupported);
 
-	if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
